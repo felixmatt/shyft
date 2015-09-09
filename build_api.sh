35ae62b50c6eb5e7fa394f3fce95734802db9d75
Ola Skavhaug <ola@xal.no>
2015-09-09 13:54:13 +0200

    OSP-4: Add pt_ss_k method stack to build script, and let it report time.

--- build_api.sh
+++ build_api.sh
@@ -1,5 +1,32 @@
-echo "updating api.py and api_wrap.cxx using swig"
-cd api/python && make -f Makefile.swig_run
-echo "compiling and linking the c++ part of the api, the _api.so shared library"
-cd ../../shyft && g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o _api.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/api_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack && g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o _pt_gs_k.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_gs_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack 
+#!/bin/sh
 
+set -e
+tf="%e"
+cd api/python 
+echo -n "Generating Python interface using swig"
+usecs=`time -f $tf -q make -f Makefile.swig_run 2>&1 > /dev/null`
+echo ", took $usecs seconds"
+t_secs=$usecs
+
+cd ../../shyft 
+echo -n "Compiling and linking _api.so, the c++ Python wrapper code for api"
+usecs=`time -f $tf -q g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o _api.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/api_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack 2>&1 > /dev/null`
+echo ", took $usecs seconds"
+t_secs=`echo "$t_secs + $usecs" | bc`
+
+echo -n "compiling and linking _pt_gs_k.so, the c++ Python wrapper code for the pt_gs_k method stack"
+usecs=`time -f $tf -q g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o _pt_gs_k.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_gs_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack 2>&1 > /dev/null`
+echo ", took $usecs seconds"
+t_secs=`echo "$t_secs + $usecs" | bc`
+
+echo -n "compiling and linking _pt_ss_k.so, the c++ Python wrapper code for the pt_ss_k method stack"
+usecs=`time -f $tf -q g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o _pt_ss_k.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_ss_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack 2>&1 > /dev/null`
+echo ", took $usecs seconds"
+t_secs=`echo "$t_secs + $usecs" | bc`
+
+mins=`echo "$t_secs / 60" | bc`
+secs=`echo "$t_secs % 60" | bc`
+echo Total time: ${mins}m ${secs}s
+
+# Back where we started
+cd ..
