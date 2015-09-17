0f63eaf96a129aacdf9504bde0dc5976b644abf1
Ola Skavhaug <ola@xal.no>
2015-09-17 13:08:50 +0200

    OSP-6: Add non-zero constructor for land type fractions

--- core/geo_cell_data.h
+++ core/geo_cell_data.h
@@ -4,23 +4,54 @@ namespace shyft {
     namespace core {
 
         /** \brief LandTypeFractions are used to describe 'type of land'
-         *   like glacier,lake,reservoir and forest
-         *   It is a designed as a part of GeoCellData (could be nested, but we take python/swig into consideration)
-         *   It's of course questionable, since we could have individual models for each
-         *   type of land, - but current approach is to use a cell-area, and then describe
-         *   fractional properties
+         *   like glacier, lake, reservoir and forest.
+         *
+         *   It is designed as a part of GeoCellData (could be nested, but 
+         *   we take python/swig into consideration). It's of course 
+         *   questionable, since we could have individual models for each type
+         *   of land, - but current approach is to use a cell-area, and then
+         *   describe fractional properties.
          */
         struct land_type_fractions {
-            land_type_fractions():glacier_(0),lake_(0),reservoir_(0),forest_(0){}
-            double glacier() const {return glacier_;}
-			double lake() const {return lake_;}   // not regulated, assume time-delay until discharge
-			double reservoir()const{return reservoir_;}// regulated, assume zero time-delay to discharge
-			double forest() const {return forest_;}
-			double unspecified() const {return 1.0 - glacier_-lake_-reservoir_-forest_;}
-            void set_fractions(double glacier,double lake,double reservoir,double forest) {
-                if(glacier+lake+reservoir+forest > 1.0  || (glacier<0.0 ||lake<0.0 || reservoir <0.0 || forest< 0.0 ) )
+            land_type_fractions()
+             : glacier_(0), lake_(0), reservoir_(0), forest_(0) {}
+
+            /** \brief LandTypeFraction constructor
+             *
+             *  Construct the land type fractions based on the sizes of the 
+             *  different land types. Each size should be greater or equal to 
+             *  zero.
+             */
+            land_type_fractions(double glacier_size, double lake_size, 
+                               double reservoir_size, double forest_size, 
+                               double unspecified_size) {
+                // TODO: Is this needed?
+                glacier_size = std::max(0.0, glacier_size);
+                lake_size = std::max(0.0, lake_size);
+                reservoir_size = std::max(0.0, reservoir_size);
+                forest_size = std::max(0.0, forest_size);
+                unspecified_size = std::max(0.0, unspecified_size);
+                const double sum = glacier_size + lake_size + reservoir_size
+                                   + forest_size + unspecified_size;
+                if (sum > 0) {
+                    glacier_ = glacier_size/sum;
+                    lake_ = lake_size/sum;
+                    reservoir_ = reservoir_size/sum;
+                    forest_ = forest_size/sum;
+                }
+                else 
+                    glacier_ = lake_ = reservoir_ = forest_ = 0.0;
+            }
+            double glacier() const { return glacier_; }
+			double lake() const { return lake_; }         // not regulated, assume time-delay until discharge
+			double reservoir()const{ return reservoir_; } // regulated, assume zero time-delay to discharge
+			double forest() const { return forest_; }
+			double unspecified() const { return 1.0 - glacier_ - lake_ - reservoir_ - forest_; }
+            void set_fractions(double glacier, double lake, double reservoir, double forest) {
+                if (glacier + lake + reservoir + forest > 1.0  || 
+                    (glacier < 0.0 || lake < 0.0 || reservoir < 0.0 || forest < 0.0))
                    throw std::invalid_argument("LandTypeFractions:: must be >=0.0 and sum <= 1.0");
-                glacier_=glacier;lake_=lake;reservoir_=reservoir;forest_=forest;
+                glacier_ = glacier; lake_ = lake; reservoir_ = reservoir; forest_ = forest;
             }
           private:
 			double glacier_;
@@ -28,6 +59,8 @@ namespace shyft {
 			double reservoir_;// regulated, assume zero time-delay to discharge
 			double forest_;
         };
+
+
         const double default_radiation_slope_factor=0.9;
 
         /** \brief geo_cell_data represents common constant geo_cell properties across several possible models and cell assemblies.
