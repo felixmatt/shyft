c574371ae6f8d6322926e38cff16f06056e421db
sigbjorn <sigbjorn.helset@gmail.com>
2015-09-20 21:54:14 +0200

    clean-up code and comments added

--- shyft/repository/yaml_state_repository.py
+++ shyft/repository/yaml_state_repository.py
@@ -1,70 +1,19 @@
-#from __future__ import print_function
 from __future__ import absolute_import
 
 from .interfaces import StateInfo, StateRepository
 
-#from shyft.orchestration.state import load_state_from_yaml_string
 import os
 from glob import glob
 import yaml
-from shyft.api import Calendar,UtcPeriod,YMDhms
-from shyft.api.pt_gs_k import PTGSKStateIo
-
+from shyft.api import Calendar,YMDhms
+from shyft.api.pt_gs_k import PTGSKStateIo #if different state-io is needed, they can be injected into the constructor, this one is provided for default argument
 
 class StateRepositoryError(Exception): pass
 
 
-def combine_conditions(c1, c2):
-    """Simple combiner of lazy evaluation of boolean expressions.""" 
-    return lambda x: c1(x) and c2(x)
-
-
-class TimeCondition(object):
-    """Overload comparision operators to return a callable that can be used for filtering during state entry searches."""
-
-    def __lt__(self, utc_timestamp):
-        return lambda x: x.utc_timestamp < utc_timestamp
-
-    def __gt__(self, utc_timestamp):
-        return lambda x: x.utc_timestamp > utc_timestamp
-
-    def __le__(self, utc_timestamp):
-        return lambda x: x.utc_timestamp <= utc_timestamp
-
-    def __ge__(self, utc_timestamp):
-        return lambda x: x.utc_timestamp >= utc_timestamp
-
-def save_state_as_yaml_file(state, filename, **params):
-    #print("write to yaml state filename:", filename)
-    with open(filename, "w") as f:
-        f.write(yaml.dump(state, **params))
-
-def load_state_from_yaml_file(filename):
-    #print("read from yaml state filename:", filename)
-    with open(filename, "r") as f:
-        contents = yaml.load(f.read())
-        return contents
-
-def get_state_file_list(state_directory,glob_pattern):
-    full_path=""
-    if os.path.isabs(state_directory):
-        full_path = state_directory
-    else:
-        cwd = os.getcwd()
-        if os.path.isdir(os.path.join(cwd, state_directory)):
-            full_path = os.path.realpath(os.path.join(cwd, state_directory))
-        elif os.path.isdir(os.path.join(os.path.realpath(__file__), state_directory)):
-            full_path = os.path.realpath(os.path.join(os.path.realpath(__file__, state_directory)))
-        else:
-            raise StateRepositoryError("Can not find directory {}, please check your configuration setup.".format(state_directory))
-        
-    if glob_pattern is None:
-        glob_pattern = "*.yaml"
-
-    return glob(os.path.join(full_path, glob_pattern))
-
 class State(object):
-    """ persisted state in the yaml-file """
+    """ module internal only, persisted state in the yaml-file, kept equal to orchestration for backward compatibility
+    """
 
     def __init__(self, state_list, utc_timestamp=None, tags=None):
         self.data = {"utc_timestamp": utc_timestamp,
@@ -116,6 +65,36 @@ class YamlStateRepository(StateRepository):
         self._directory_path=directory_path
         self._sio=state_serializer
         self._file_pattern=file_pattern
+    
+    def _get_state_file_list(self,state_directory,glob_pattern):
+        full_path=""
+        if os.path.isabs(state_directory):
+            full_path = state_directory
+        else:
+            cwd = os.getcwd()
+            if os.path.isdir(os.path.join(cwd, state_directory)):
+                full_path = os.path.realpath(os.path.join(cwd, state_directory))
+            elif os.path.isdir(os.path.join(os.path.realpath(__file__), state_directory)):
+                full_path = os.path.realpath(os.path.join(os.path.realpath(__file__, state_directory)))
+            else:
+                raise StateRepositoryError("Can not find directory {}, please check your configuration setup.".format(state_directory))
+            
+        if glob_pattern is None:
+            glob_pattern = "*.yaml"
+
+        return glob(os.path.join(full_path, glob_pattern))
+
+    def _save_state_as_yaml_file(self,state, filename, **params):
+        #print("write to yaml state filename:", filename)
+        with open(filename, "w") as f:
+            f.write(yaml.dump(state, **params))
+    
+    def _load_state_from_yaml_file(self,filename):
+        #print("read from yaml state filename:", filename)
+        with open(filename, "r") as f:
+            contents = yaml.load(f.read())
+            return contents
+   
 
     def _unique_id(self,region_model_id,utc_timestamp):
         """ given region_model_id and utc_timestamp, return unique id (filename within directory) 
@@ -167,7 +146,7 @@ class YamlStateRepository(StateRepository):
         if utc_timestamp_criteria is not None and region_model_id_criteria is None:
             raise StateRepositoryError("You have to specify a region_model_id AND a utc_timestamp_critera")
         
-        file_list=get_state_file_list(self._directory_path,self._file_pattern)
+        file_list=self._get_state_file_list(self._directory_path,self._file_pattern)
         res=[]
         for filename in file_list:
             si= self._state_info_from_filename(os.path.basename(filename))
@@ -191,7 +170,7 @@ class YamlStateRepository(StateRepository):
         """
         return the state for a specified state_id, - the returned object/type can be passed directly to the region-model 
         """
-        state= load_state_from_yaml_file(os.path.join(self._directory_path,state_id))
+        state= self._load_state_from_yaml_file(os.path.join(self._directory_path,state_id))
         return self._sio.vector_from_string(state.state_list)
     
     def put_state(self, region_model_id,utc_timestamp,region_model_state,tags=None):
@@ -203,7 +182,7 @@ class YamlStateRepository(StateRepository):
         state_id=self._unique_id(region_model_id,utc_timestamp)
         #TODO: save state here
         persisted_form= State(self._sio.to_string(region_model_state),utc_timestamp,tags)
-        save_state_as_yaml_file(persisted_form,os.path.join(self._directory_path,state_id))
+        self._save_state_as_yaml_file(persisted_form,os.path.join(self._directory_path,state_id))
         return state_id
     
     def delete_state(self, state_id):
