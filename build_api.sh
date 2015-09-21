6f434a5c6a57cb97f94832f06b1e8207a9ac5b48
Ola Skavhaug <ola@xal.no>
2015-09-21 14:07:17 +0200

    OSP-6: Use c++1y standard when compiling

--- build_api.sh
+++ build_api.sh
@@ -15,21 +15,21 @@ t_secs=$usecs
 cd ../../shyft 
 echo -n "Compiling and linking _api.so, the c++ Python wrapper code for api"
 tic=`date ${tf}` 
-g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/___init__.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/__init___wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
+g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/___init__.so -pthread -s -fPIC -std=c++1y -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/__init___wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
 usecs=`echo $(date ${tf}) - $tic | bc`
 echo ", took $usecs seconds"
 t_secs=`echo "$t_secs + $usecs" | bc`
 
 echo -n "compiling and linking _pt_gs_k.so, the c++ Python wrapper code for the pt_gs_k method stack"
 tic=`date ${tf}` 
-g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/_pt_gs_k.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_gs_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
+g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/_pt_gs_k.so -pthread -s -fPIC -std=c++1y -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_gs_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
 usecs=`echo $(date ${tf}) - $tic | bc`
 echo ", took $usecs seconds"
 t_secs=`echo "$t_secs + $usecs" | bc`
 
 echo -n "compiling and linking _pt_ss_k.so, the c++ Python wrapper code for the pt_ss_k method stack"
 tic=`date ${tf}` 
-g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/_pt_ss_k.so -pthread -s -fPIC -std=c++11 -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_ss_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
+g++ -shared -L/usr/lib/python2.7 -L/usr/local/lib64 -o api/_pt_ss_k.so -pthread -s -fPIC -std=c++1y -DARMA_DONT_USE_WRAPPER -I/usr/include/python2.7 -I.. -I../../dlib -O3 -lblas -llapack ../api/python/pt_ss_k_wrap.cxx  ../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp -lblas -llapack
 usecs=`echo $(date ${tf}) - $tic | bc`
 echo ", took $usecs seconds"
 t_secs=`echo "$t_secs + $usecs" | bc`
