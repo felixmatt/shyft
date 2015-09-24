4de4980743256f3cda7658e62be49f5a8d536eec
sigbjorn <sigbjorn.helset@gmail.com>
2015-09-24 09:10:44 +0200

    yam_geo_location_repository
    In case gis-service lacks points, we can use yaml-file.
    This implements a yaml-based repository with files.

---
+++ shyft/repository/service/yaml_geo_location_repository.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import yaml
+from .ssa_geo_ts_repository import GeoLocationRepository
+
+class YamlGeoLocationError(Exception): pass
+
+class YamlGeoLocationRepository(GeoLocationRepository):
+    """
+    Provide a yaml-based key-location map for gis-identites not available(yet)
+
+    """
+
+    def __init__(self, yaml_file_dir):
+        """
+        Parameters
+        ----------
+        yam_file_dir:string
+            path to directory where files 
+            pt_locations-32632.yml (UTM32N) and
+            pt_locations-32633.yml (UTM33N)
+            pt_locations-<epgs_id>.yml
+        """
+        self.file_dir = yaml_file_dir
+
+    def _filename_of(self,epgs_id):
+        return "pt_locations-{}".format(epgs_id);
+    
+    def read_location_dict(self,epgs_id):
+        full_name=path.join(self._file_dir,self._filename_of(epgs_id))
+        with open(full_name, 'r') as f:
+            return yaml.load(f)
+
+    def get_locations(self, location_id_list,epgs_id=32632):
+        loc_dict = self.read_forecast_series_loc()
+        locations = {}
+        for index in self.indices:
+            if loc_dict.get(index) != None:
+                locations[index] = tuple(loc_dict[index])
+            else:
+                raise YamlGeoLocationError("Could not get location of geo point-id!")
+        return locations
+        
