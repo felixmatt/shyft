4aab8349ad699517b6523fb004fa7fb1bb397de7
sigbjorn <sigbjorn.helset@gmail.com>
2015-09-25 15:34:22 +0200

    Starting work on service base gis_region_model_repository
    Rename service/gis_data to,
    prepare initial tests

---
+++ shyft/tests/test_gis_region_model_repository.py
@@ -0,0 +1,9 @@
+﻿import unittest
+
+class TestGisRegionModelRepository(unittest.TestCase):
+    
+    def verify_using_existing_live_services_at_statkraft(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
