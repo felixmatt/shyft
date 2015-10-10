841b6fd5cea5ba54429a77356d037eaa60e58fcf
sigbjorn <sigbjorn.helset@gmail.com>
2015-10-10 12:00:15 +0200

    orchestration2/orchestration functionality -> orchestration, cleaning up, since orchestration/simulator now covers simulations functionality, and forecast/ens and calibration will be added based on repositories provided

--- shyft/orchestration2/generic/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Here it is where the generic configuration classes live.
-"""
-
-from .config import Config
--- shyft/orchestration2/netcdf/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from shyft.repository.netcdf.source_dataset import SourceDatasetRepository
-from .region import Region
-from .simulation_output import SimulationOutput
-from .target import Target
