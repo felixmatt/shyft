bd96dee2a4a4dd434bf01f7a95345a9089fc62f5
Ola Skavhaug <ola@xal.no>
2015-11-06 16:27:10 +0100

    Ensemble demo with percentile plotting in place

---
+++ shyft/orchestration/plotting.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib import ticker
+from shyft.api import Timeaxis
+from shyft.api import deltahours
+
+
+def plot_np_percentiles(time, percentiles, base_color=1.0, alpha=0.5, plw=0.5, linewidth=1, mean_color=0.0, label=None):
+    if base_color is not None:
+        if not isinstance(base_color, np.ndarray):
+            if isinstance(base_color, (int, float)):
+                base_color = 3*[base_color]
+            base_color = np.array(base_color)
+    if not isinstance(mean_color, np.ndarray):
+        if isinstance(mean_color, (int, float)):
+            mean_color = 3*[mean_color]
+        mean_color = np.array(mean_color)
+    percentiles = list(percentiles)
+    num_intervals = len(percentiles)//2
+    f_handles = []
+    for i in range(num_intervals):
+        facecolor = list((1.0 - float(i+1)/(num_intervals + 1))*base_color) + [alpha] if base_color is not None else 4*[1.]
+        f_handles.append(plt.fill_between(time, percentiles[i], percentiles[-(i+1)],
+                         edgecolor=(0, 0, 0, 0),
+                         facecolor=facecolor))
+    linewidths = len(percentiles)*[plw]
+    linecols = len(percentiles)*[(0.7, 0.7, 0.7, 1.0)]
+    labels = len(percentiles)*[None]
+    if len(percentiles) % 2:
+        mid = len(percentiles)//2
+        linewidths[mid] = linewidth
+        linecols[mid] = mean_color
+        labels[mid] = label
+    handles = []
+    for p, lw, lc, label in zip(percentiles, linewidths, linecols, labels):
+        h, = plt.plot(time, p, linewidth=lw, color=lc, label=label)
+        handles.append(h)
+    if len(percentiles) % 2:
+        mean_h = handles.pop(len(handles)//2)
+        handles = [mean_h] + handles
+    return handles + f_handles
+
+
+def set_display_time_axis(ta, cal, n_xticks=10, format="W-WY"):
+    if isinstance(ta, Timeaxis):
+        dt = ta.delta()
+        start = ta.start()
+        t = [start + i*dt for i in range(ta.size())]
+    else:
+        t = ta[:]
+    ticks = ([t[int(round(_))] for _ in np.linspace(0, len(t) - 1, n_xticks)])
+    ticks[-1] = t[-1]
+    def convert(t_utc):
+        ymd = cal.calendar_units(t_utc)
+        return "{:04d}.{:02d}.{:02d}:{:02d}".format(ymd.year, ymd.month, ymd.day, ymd.hour)
+    handles = plt.xticks(ticks, [convert(_t) for _t in ticks], rotation="vertical")
+    plt.subplots_adjust(bottom=0.15)
+    plt.xlabel("Time in {} coordinates.".format("UTC"))
+    plt.xlim(ticks[0], ticks[-1])
+    return handles
+
+
+def set_utc_time_formatter(ta, cal):
+    ax = plt.gca()
+    fig = plt.gcf()
+    def format_date(x, pos=None):
+        t_utc = cal.trim(int(round(x)), deltahours(1))
+        ymd = cal.calendar_units(t_utc)
+        return "{:04d}.{:02d}.{:02d}:{:02d}".format(ymd.year, ymd.month, ymd.day, ymd.hour)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    fig.autofmt_xdate()
+    return ax
--- shyft/tests/tistel_demo.py
+++ shyft/tests/tistel_demo.py
@@ -16,8 +16,10 @@ from shyft_config import tistel
 from shyft.repository.default_state_repository import DefaultStateRepository
 from shyft.repository.service.gis_region_model_repository import RegionModelConfig
 from shyft.repository.service.gis_region_model_repository import GisRegionModelRepository
-from shyft.orchestration.simulator import SimpleSimulator as Simulator
 from shyft.repository.service.ssa_smg_db import SmGTsRepository, PROD, FC_PROD
+from shyft.orchestration.plotting import plot_np_percentiles
+from shyft.orchestration.plotting import set_utc_time_formatter
+from shyft.orchestration.simulator import SimpleSimulator as Simulator
 
 
 def create_tistel_simulator(geo_ts_repository):
@@ -49,36 +51,42 @@ def burn_in_state(t_start, t_stop, q_obs_m3s_ts):
     ptgsk.run(time_axis, state_repos.get_state(0))
     # Go back in time (to t_start) and adjust q with observed discharge at that time.
     # This will give us a good initial state at t_start
-    return ptgsk, tistel_tukle_state(ptgsk, ptgsk.reg_model_state(), t_start, q_obs_m3s_ts)
+    return ptgsk, adjust_simulator_state(ptgsk, t_start, q_obs_m3s_ts)
 
 
-def tistel_tukle_state(sim, state, t, q_obs):
-    q_obs_m3s = q_obs.value(q_obs.index_of(t))
-    return sim.discharge_adjusted_state(q_obs_m3s, state)
+def adjust_simulator_state(sim, t, q_obs):
+    return sim.discharge_adjusted_state(q_obs.value(q_obs.index_of(t)))
 
 
-def plot_results(ptgsk, q_obs_m3s_ts):
-    discharge = ptgsk.region_model.statistics.discharge([0])
-    temp = ptgsk.region_model.statistics.temperature([0])
-    precip = ptgsk.region_model.statistics.precipitation([0])
+def plot_results(ptgsk, q_obs=None):
+    if ptgsk is not None:
+        plt.subplot(3,1,1)
+        discharge = ptgsk.region_model.statistics.discharge([0])
+        temp = ptgsk.region_model.statistics.temperature([0])
+        precip = ptgsk.region_model.statistics.precipitation([0])
 
-    # Results on same time axis, so we only need one
-    ts = [discharge.time(i) for i in range(discharge.size())]
+        # Results on same time axis, so we only need one
+        ts = [discharge.time(i) for i in range(discharge.size())]
     
-    plt.subplot(3,1,1)
-    plt.plot(ts, np.array(discharge.v))
-    plt.hold(1)
-    ots = [q_obs_m3s_ts.time(i) for i in range(q_obs_m3s_ts.size())]
-    ovs = [q_obs_m3s_ts.value(i) for i in range(q_obs_m3s_ts.size())]
-    plt.plot(ots, ovs)
-    plt.legend(["Simulated Discharge", "Observed Discharge"])
-    plt.subplot(3, 1, 2)
-    plt.plot(ts, np.array(temp.v))
-    plt.subplot(3, 1, 3)
-    plt.plot(ts, np.array(precip.v))
-    plt.interactive(1)
-    plt.show()
-    return ptgsk
+        plt.plot(ts, np.array(discharge.v))
+    if q_obs is not None:
+        ots = [q_obs.time(i) for i in range(q_obs.size())]
+        ovs = [q_obs.value(i) for i in range(q_obs.size())]
+        plt.plot(ots, ovs, linewidth=2, color='k')
+    if ptgsk is not None:
+        plt.subplot(3, 1, 2)
+        plt.plot(ts, np.array(temp.v))
+        plt.subplot(3, 1, 3)
+        plt.plot(ts, np.array(precip.v))
+
+
+def plot_percentiles(sim, percentiles, time_axis):
+    discharges = [s.region_model.statistics.discharge([0]) for s in sim]
+    ts = np.array([discharges[0].time(i) for i in range(discharges[0].size())])
+    all_discharges = np.array([d.v for d in discharges])
+    perc_arrs = [a for a in np.percentile(all_discharges, percentiles, 0)]
+    h = plot_np_percentiles(ts, perc_arrs)
+    ax = set_utc_time_formatter(time_axis, Calendar()) 
 
 
 def demo_tistel_forecast():
@@ -96,12 +104,53 @@ def demo_tistel_forecast():
     ptgsk, initial_state = burn_in_state(t_start, utc.time(YMDhms(2012, 9, 1)), q_obs_m3s_ts)
     ptgsk.run(obs_time_axis, initial_state)
     plot_results(ptgsk, q_obs_m3s_ts)  
-    current_state = tistel_tukle_state(ptgsk, ptgsk.reg_model_state(), t_fc_start, q_obs_m3s_ts)
+
+    current_state = adjust_simulator_state(ptgsk, t_fc_start, q_obs_m3s_st)
+
     ptgsk_fc = create_tistel_simulator(tistel.arome_repository(tistel.grid_spec, t_fc_start))
     ptgsk_fc.run(fc_time_axis, current_state)
     plt.figure()
     q_obs_m3s_ts = observed_tistel_discharge(fc_time_axis.total_period())
     plot_results(ptgsk_fc, q_obs_m3s_ts)
-    
+    plt.interactive(1)
+    plt.show()
+
+
+
+def demo_tistel_ensemble():
+    utc = Calendar()
+    t_start = utc.time(YMDhms(2011, 9, 1))
+    t_fc_ens_start = utc.time(YMDhms(2015, 7, 26))
+    disp_start = utc.time(YMDhms(2015, 7, 20))
+    dt = deltahours(1)
+    n_obs = int(round((t_fc_ens_start - t_start)/dt))
+    n_fc_ens = 30
+    n_disp = int(round(t_fc_ens_start - disp_start)/dt) + n_fc_ens + 24*7
+
+    obs_time_axis = Timeaxis(t_start, dt, n_obs + 1)
+    fc_ens_time_axis = Timeaxis(t_fc_ens_start, dt, n_fc_ens)
+    display_time_axis = Timeaxis(disp_start, dt, n_disp)
+
+    q_obs_m3s_ts = observed_tistel_discharge(obs_time_axis.total_period())
+    ptgsk, initial_state = burn_in_state(t_start, utc.time(YMDhms(2012, 9, 1)), q_obs_m3s_ts)
+
+    ptgsk.run(obs_time_axis, initial_state)
+    current_state = adjust_simulator_state(ptgsk, t_fc_ens_start, q_obs_m3s_ts)
+    ens_repos = tistel.arome_ensemble_repository(tistel.grid_spec)
+    ptgsk_fc_ens = create_tistel_simulator(ens_repos)
+    sims = ptgsk_fc_ens.create_ensembles(fc_ens_time_axis, t_fc_ens_start, current_state)
+    for sim in sims:
+        sim.simulate()
+    plt.hold(1)
+    percentiles = [10, 25, 50, 75, 90]
+    plot_percentiles(sims, percentiles, display_time_axis)
+    q_obs_m3s_ts = observed_tistel_discharge(display_time_axis.total_period())
+    plot_results(None, q_obs_m3s_ts)
+    plt.interactive(1)
+    plt.legend([str(p) for p in percentiles] + ["Obs"])
+    plt.show()
+     
+
 if __name__ == "__main__":
-    demo_tistel_forecast()
+    #demo_tistel_forecast()
+    sims = demo_tistel_ensemble()
