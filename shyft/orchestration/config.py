96516ff0d279a282e1f52efe1786335e3eb5cdf6
Francesc Alted <faltet@gmail.com>
2015-11-27 09:05:08 +0100

    Read model, region and datasets config files

--- shyft/orchestration/config.py
+++ shyft/orchestration/config.py
@@ -2,11 +2,15 @@
 Utilities for reading configurations for SHyFT simulations and calibrations.
 """
 
+import os
 from datetime import datetime
-import yaml
-from shyft import api
 
+import yaml
 import numpy as np
+
+from shyft import api
+from shyft.repository.netcdf import yaml_config
+
 
 utc_calendar = api.Calendar()
 """Invariant global calendar in UTC."""
@@ -18,6 +22,10 @@ def utctime_from_datetime(dt):
     nsec = np.array([dt], dtype="datetime64[s]").astype(np.long)[0]
     dt = datetime.fromtimestamp(nsec)
     return utc_calendar.time(api.YMDhms(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second))
+
+
+class ConfigError(Exception):
+    pass
 
 
 class OrchestrationConfig(object):
@@ -32,10 +40,36 @@ class OrchestrationConfig(object):
             config = yaml.load(cfg_file)[config_section]
         # Expose all keys in yaml file as attributes
         self.__dict__.update(config)
+
+        # Check validity of some attributes
+        if not hasattr(self, "config_dir"):
+            raise ConfigError("'config_dir' must be present in config section")
+        if not (os.path.isdir(self.config_dir) and
+                os.path.isabs(self.config_dir)):
+            raise ConfigError("'config_dir' must be an absolute directory")
+        if not hasattr(self, "data_dir"):
+            raise ConfigError("'data_dir' must be present in config section")
+        if not (os.path.isdir(self.data_dir) and
+                os.path.isabs(self.data_dir)):
+            raise ConfigError("'data_dir' must be an absolute directory")
+
         # Create a time axis
         self.start_time = utctime_from_datetime(self.start_datetime)
         self.time_axis = api.Timeaxis(
             self.start_time, self.run_time_step, self.number_of_steps)
+        # Get the region model in API
+        self.model_t = getattr(api.pt_gs_k, self.model_t)
+
+        # Read region, model and datasets config files
+        region_config_file = os.path.join(
+            self.config_dir, self.region_config_file)
+        self.region_config = yaml_config.RegionConfig(region_config_file)
+        model_config_file = os.path.join(
+            self.config_dir, self.model_config_file)
+        self.model_config = yaml_config.ModelConfig(model_config_file)
+        datasets_config_file = os.path.join(
+            self.config_dir, self.datasets_config_file)
+        self.datasets_config = yaml_config.YamlContent(datasets_config_file)
 
     def __repr__(self):
         srepr = "%s::%s(" % (self.__class__.__name__, self._config_section)
