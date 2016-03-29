15e18ffcde862124edefa8c4d161bdd3ac1f6ae1
sigbjorn <sigbjorn.helset@gmail.com>
2016-03-29 21:17:58 +0200

    added conditional skip test if cf_cell.nc is missing

--- shyft/tests/test_config_simulator.py
+++ shyft/tests/test_config_simulator.py
@@ -5,8 +5,12 @@ from shyft.repository.default_state_repository import DefaultStateRepository
 from shyft.orchestration.configuration.yaml_configs import YAMLSimConfig
 from shyft.orchestration.simulators.config_simulator import ConfigSimulator
 
+cell_nc_file= path.join(path.dirname(__file__),"..","..","..","shyft-data","netcdf","orchestration-testdata","cell_data.nc")
+
 class ConfigSimulationTestCase(unittest.TestCase):
 
+    @unittest.skipIf(not path.exists(cell_nc_file),
+                     "missing file {0} \n, test ignored ".format(cell_nc_file))
     def test_run_geo_ts_data_config_simulator(self):
         # set up configuration
         config_dir = path.join(path.dirname(__file__), "netcdf")
