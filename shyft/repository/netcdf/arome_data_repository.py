5c3267690c874fe3f5339ed82331c7ac61831479
yisak <yisak.abdella@gmail.com>
2016-05-09 18:20:56 +0200

    Enabled use of filename patterns for fetching timseries.

--- shyft/repository/netcdf/arome_data_repository.py
+++ shyft/repository/netcdf/arome_data_repository.py
@@ -153,7 +153,8 @@ class AromeDataRepository(interfaces.GeoTsRepository):
         filename = self._filename
 
         if not path.isfile(filename):
-            raise AromeDataRepositoryError("File '{}' not found".format(filename))
+            #raise AromeDataRepositoryError("File '{}' not found".format(filename))
+            filename = self._get_files(utc_period.start, "_(\d{8})_(\d{2}).nc$")
         with Dataset(filename) as dataset:
             return self._get_data_from_dataset(dataset, input_source_types,
                                                utc_period, geo_location_criteria)
