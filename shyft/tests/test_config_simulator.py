9935b23111618d7de5d782d0f7ab08fada9f228b
Ludovic Pochon-Guerin <Ludovic.Pochon-Guerin@statkraft.com>
2016-10-20 15:04:44 +0200

    Add relevant comments to find the config files needed for the test

--- shyft/tests/test_config_simulator.py
+++ shyft/tests/test_config_simulator.py
@@ -5,32 +5,33 @@ from shyft.repository.default_state_repository import DefaultStateRepository
 from shyft.orchestration.configuration.yaml_configs import YAMLSimConfig
 from shyft.orchestration.simulators.config_simulator import ConfigSimulator
 from shyft.api import IntVector
-cell_nc_file= path.join(path.dirname(__file__),"..","..","..","shyft-data","netcdf","orchestration-testdata","cell_data.nc")
+
 
 class ConfigSimulationTestCase(unittest.TestCase):
 
-    # @unittest.skipIf(not path.exists(cell_nc_file),"missing file {0} \n, test ignored ".format(cell_nc_file))
     def test_run_geo_ts_data_config_simulator(self):
-        # set up configuration
+        # These config files are versioned in shyft git
         config_dir = path.join(path.dirname(__file__), "netcdf")
-        config_file = path.join(config_dir,"neanidelva_simulation.yaml")
+        config_file = path.join(config_dir, "neanidelva_simulation.yaml")
         config_section = "neanidelva"
         cfg = YAMLSimConfig(config_file, config_section)
 
-        # get a simulator
+        # These config files are versioned in shyft-data git. Read from ${SHYFTDATA}/netcdf/orchestration-testdata/
+        # TODO: Put all config files needed to run this test under the same versioning system (shyft git)
         simulator = ConfigSimulator(cfg)
-
         n_cells = simulator.region_model.size()
         state_repos = DefaultStateRepository(simulator.region_model.__class__, n_cells)
         simulator.run(cfg.time_axis, state_repos.get_state(0))
         cids = IntVector()
         discharge = simulator.region_model.statistics.discharge(cids)
-        #  regression test on discharge values
+
+        # Regression tests on discharge values
         self.assertAlmostEqual(discharge.values[0],0.1961,3)
         self.assertAlmostEqual(discharge.values[3],2.7582,3)
         self.assertAlmostEqual(discharge.values[6400],58.9381,3)
         self.assertAlmostEqual(discharge.values[3578],5.5069,3)
-        # regression test on geo fractions
+
+        # Regression tests on geo fractions
         self.assertAlmostEqual(simulator.region_model.cells[0].geo.land_type_fractions_info().unspecified(),1.0,3)
         self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().unspecified(),0.1433,3)
         self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().forest(),0.0,3)
