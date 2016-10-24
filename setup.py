45711797a10604187c4f4952634b1f9832bf7988
Ludovic Pochon-Guerin <Ludovic.Pochon-Guerin@statkraft.com>
2016-10-24 07:47:58 +0200

    Disable import tests when building conda package as it expects the package to be installed locally

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import sys
 
 # Build shyft using the build_script
 # Create package using 'python setup.py bdist_conda'
+# http://conda.pydata.org/docs/bdist_conda.html
 
 print('Building SHyFT')
 
@@ -56,8 +57,7 @@ setup(
     license='LGPL v3',
     packages=find_packages(),
     package_data={'shyft': ['api/*.so', 'api/*.pyd', 'api/pt_gs_k/*.pyd', 'api/pt_gs_k/*.so', 'api/pt_hs_k/*.pyd', 'api/pt_hs_k/*.so', 'api/pt_ss_k/*.pyd', 'api/pt_ss_k/*.so', 'api/hbv_stack/*.pyd', 'api/hbv_stack/*.so', 'tests/netcdf/*']},
-    entry_points={
-        
-    },
+    entry_points={},
+    conda_import_tests=False,
     requires=["numpy", "nose", "netCDF4"]
 )
