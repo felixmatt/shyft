97170b2d29dcecb63fd380224abf09150fd2aaeb
sigbjorn <sigbjorn.helset@gmail.com>
2016-11-13 02:30:31 +0100

    fixup and simplify datetime to utctime using only calendar coordinate and Calendar to do the exchange

--- shyft/tests/test_config_simulator.py
+++ shyft/tests/test_config_simulator.py
@@ -4,17 +4,24 @@ import unittest
 from shyft.repository.default_state_repository import DefaultStateRepository
 from shyft.orchestration.configuration.yaml_configs import YAMLSimConfig
 from shyft.orchestration.simulators.config_simulator import ConfigSimulator
-from shyft.api import IntVector
+from shyft.api import IntVector, Calendar
+from shyft.orchestration.config import utctime_from_datetime
+import datetime as dt
 
 
 class ConfigSimulationTestCase(unittest.TestCase):
+    def test_utctime_from_datetime(self):
+        utc = Calendar()
+        dt1 = dt.datetime(2015, 6, 1, 2, 3, 4)
+        t1 = utctime_from_datetime(dt1)
+        self.assertEqual(t1, utc.time(2015, 6, 1, 2, 3, 4))
 
     def test_run_geo_ts_data_config_simulator(self):
         # These config files are versioned in shyft git
         config_dir = path.join(path.dirname(__file__), "netcdf")
         config_file = path.join(config_dir, "neanidelva_simulation.yaml")
         config_section = "neanidelva"
-        cfg = YAMLSimConfig(config_file, config_section,overrides={'config': {'number_of_steps':168}})
+        cfg = YAMLSimConfig(config_file, config_section, overrides={'config': {'number_of_steps': 168}})
 
         # These config files are versioned in shyft-data git. Read from ${SHYFTDATA}/netcdf/orchestration-testdata/
         # TODO: Put all config files needed to run this test under the same versioning system (shyft git)
@@ -28,18 +35,18 @@ class ConfigSimulationTestCase(unittest.TestCase):
         # Regression tests on discharge values
         self.assertAlmostEqual(discharge.values[0], 0.1961, 3)
         self.assertAlmostEqual(discharge.values[3], 2.748813, 3)  #
-        #x self.assertAlmostEqual(discharge.values[6400], 58.8385, 3) # was 58.9381,3 before glacier&fractions adjustments
-        #x self.assertAlmostEqual(discharge.values[3578],5.5069,3)
+        # x self.assertAlmostEqual(discharge.values[6400], 58.8385, 3) # was 58.9381,3 before glacier&fractions adjustments
+        # x self.assertAlmostEqual(discharge.values[3578],5.5069,3)
         # glacier_melt, not much, but enough to test
-        #x self.assertAlmostEqual(simulator.region_model.gamma_snow_response.glacier_melt(cids).values.to_numpy().max(),0.201625547258,4)
+        # x self.assertAlmostEqual(simulator.region_model.gamma_snow_response.glacier_melt(cids).values.to_numpy().max(),0.201625547258,4)
         self.assertAlmostEqual(simulator.region_model.gamma_snow_response.glacier_melt(cids).values.to_numpy().max(), 0.11938204918828155, 4)
         # Regression tests on geo fractions
-        self.assertAlmostEqual(simulator.region_model.cells[0].geo.land_type_fractions_info().unspecified(),1.0,3)
-        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().unspecified(),0.1433,3)
-        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().forest(),0.0,3)
-        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().reservoir(),0.8566,3)
-        #x self.assertAlmostEqual(simulator.region_model.cells[3383].geo.land_type_fractions_info().lake(),0.7432,3)
-        #x self.assertAlmostEqual(simulator.region_model.cells[652].geo.land_type_fractions_info().glacier(),0.1351,3)
+        self.assertAlmostEqual(simulator.region_model.cells[0].geo.land_type_fractions_info().unspecified(), 1.0, 3)
+        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().unspecified(), 0.1433, 3)
+        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().forest(), 0.0, 3)
+        self.assertAlmostEqual(simulator.region_model.cells[2].geo.land_type_fractions_info().reservoir(), 0.8566, 3)
+        # x self.assertAlmostEqual(simulator.region_model.cells[3383].geo.land_type_fractions_info().lake(),0.7432,3)
+        # x self.assertAlmostEqual(simulator.region_model.cells[652].geo.land_type_fractions_info().glacier(),0.1351,3)
 
 
 if __name__ == '__main__':
