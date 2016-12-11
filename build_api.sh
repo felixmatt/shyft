6c1901f6c0856db93f0f34a5f8e545c1e8bf33a3
sigbjorn <sigbjorn.helset@gmail.com>
2016-12-11 17:55:32 +0100

    fixed build with dlib as lib

--- build_api.sh
+++ build_api.sh
@@ -26,9 +26,12 @@ armadillo_defs="${armadillo_defs:--DARMA_DONT_USE_WRAPPER -DARMA_DONT_PRINT_ERRO
 boost_libs="${boost_libs:--lboost_python3 -lboost_serialization}"
 boost_include="${boost_include:-}"
 
+dlib_lib="${dlib_lib:--ldlib}"
+dlib_include="${dlib_include:--I../../dlib}"
+
 shyft_march="${shyft_march:--march=native}"
 
-gcc_opt="-shared -L/usr/local/lib64  -s -pthread -fPIC -std=c++1y -Wno-deprecated-declarations -O3 $shyft_march -I.. -I../../dlib $armadillo_defs $boost_include $py_include"
+gcc_opt="-shared -L/usr/local/lib64  -s -pthread -fPIC -std=c++1y -Wno-deprecated-declarations -O3 $shyft_march -I.. $dlib_include $armadillo_defs $boost_include $py_include"
 shyft_common_source="../core/utctime_utilities.cpp ../core/sceua_optimizer.cpp ../core/dream_optimizer.cpp ../api/api.cpp ../api/timeseries.cpp ../core/core_serialization.cpp ../api/api_serialization.cpp"
 #"../api/boostpython/api.cpp ../api/boostpython/api_actual_evapotranspiration.cpp ../api/boostpython/api_cell_environment.cpp ../api/boostpython/api_gamma_snow.cpp ../api/boostpython/api_geo_cell_data.cpp ../api/boostpython/api_geo_point.cpp ../api/boostpython/api_hbv_snow.cpp ../api/boostpython/api_interpolation.cpp ../api/boostpython/api_kirchner.cpp ../api/boostpython/api_precipitation_correction.cpp ../api/boostpython/api_priestley_taylor.cpp ../api/boostpython/api_region_environment.cpp ../api/boostpython/api_skaugen.cpp ../api/boostpython/api_target_specification.cpp ../api/boostpython/api_time_axis.cpp ../api/boostpython/api_utctime.cpp ../api/boostpython/api_vectors.cpp"
 shyft_api_pch="../api/boostpython/boostpython_pch.h"
@@ -38,15 +41,15 @@ shyft_api_source=`ls ../api/boostpython/api*.cpp`
 echo "First building pch file"
 g++ $gcc_opt -c $shyft_api_pch -o $shyft_api_pch_out
 echo "  Compile&Link _api.so started"
-g++ $gcc_opt  -o api/_api.so    $shyft_api_source    $shyft_common_source  $armadillo_libs $boost_libs $py_lib &
+g++ $gcc_opt  -o api/_api.so    $shyft_api_source    $shyft_common_source  $armadillo_libs $boost_libs $py_lib $dlib_lib $boost_libs &
 echo "  Compile&Link _pt_gs_k.so started"
-g++ $gcc_opt  -o api/pt_gs_k/_pt_gs_k.so   ../api/boostpython/pt_gs_k.cpp   $shyft_common_source  $armadillo_libs $boost_libs $py_lib  &
+g++ $gcc_opt  -o api/pt_gs_k/_pt_gs_k.so   ../api/boostpython/pt_gs_k.cpp   $shyft_common_source  $armadillo_libs $boost_libs $py_lib $dlib_lib $boost_libs &
 echo "  Compile&Link _pt_ss_k.so started"
-g++ $gcc_opt -o api/pt_ss_k/_pt_ss_k.so    ../api/boostpython/pt_ss_k.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib &
+g++ $gcc_opt -o api/pt_ss_k/_pt_ss_k.so    ../api/boostpython/pt_ss_k.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib $dlib_lib $boost_libs &
 echo "  Compile&Link _pt_hs_k.so started"
-g++ $gcc_opt -o api/pt_hs_k/_pt_hs_k.so    ../api/boostpython/pt_hs_k.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib &
+g++ $gcc_opt -o api/pt_hs_k/_pt_hs_k.so    ../api/boostpython/pt_hs_k.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib $dlib_lib $boost_libs &
 echo "  Compile&Link _hbv_stack.so started"
-g++ $gcc_opt -o api/hbv_stack/_hbv_stack.so    ../api/boostpython/hbv_stack.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib &
+g++ $gcc_opt -o api/hbv_stack/_hbv_stack.so    ../api/boostpython/hbv_stack.cpp    $shyft_common_source  $armadillo_libs $boost_libs $py_lib $dlib_lib $boost_libs &
 
 echo -n "Waiting for the background compilations to complete..(could take some minutes)"
 wait
