de542e1afd0b489d706543caa01ccece7f137098
Sigbjørn Helset <sigbjorn.helset@statkraft.com>
2017-03-13 16:02:15 +0100

    added start for threaded perf. testing of dtss server

--- test/dtss_test.cpp
+++ test/dtss_test.cpp
@@ -27,7 +27,7 @@ dlib::logger dlog("dlib.log");
 
 TEST_SUITE("serialization");
 
-TEST_CASE("test_dlib_server") {
+TEST_CASE("dlib_server_basics") {
     dlog.set_level(dlib::LALL);
     dlib::set_all_logging_output_streams(std::cout);
     dlib::set_all_logging_levels(dlib::LALL);
@@ -63,8 +63,8 @@ TEST_CASE("test_dlib_server") {
         int port_no = 20000;
         our_server.set_listening_port(port_no);
         our_server.start_async();
-        //while(our_server.is_running()&& our_server.get_listening_port()==0)
-        //    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        //while(our_server.is_running()&& our_server.get_listening_port()==0) //because dlib do not guarantee that listening port is set
+        //    std::this_thread::sleep_for(std::chrono::milliseconds(3)); // upon return, so we have to wait until it's done
 
         //int port_no=our_server.get_listening_port();
         {
@@ -103,3 +103,81 @@ TEST_CASE("test_dlib_server") {
     }
     dlog << dlib::LINFO << "done";
 }
+TEST_CASE("dlib_server_performance") {
+    dlog.set_level(dlib::LALL);
+    dlib::set_all_logging_output_streams(std::cout);
+    dlib::set_all_logging_levels(dlib::LALL);
+    dlog << dlib::LINFO << "Starting dtss test";
+    using namespace shyft::dtss;
+    try {
+        // Tell the server to begin accepting connections.
+        calendar utc;
+        auto t = utc.time(2016, 1, 1);
+        auto dt = deltahours(1);
+        auto dt24 = deltahours(24);
+        int n = 24*365*5;
+        int n24 = n / 24;
+        shyft::time_axis::fixed_dt ta(t, dt, n);
+        api::gta_t ta24(t, dt24, n24);
+        bool throw_exception = false;
+        call_back_t cb = [ta, &throw_exception](id_vector_t ts_ids, core::utcperiod p)
+            ->ts_vector_t {
+            ts_vector_t r;r.reserve(ts_ids.size());
+            double fv = 1.0;
+            for (size_t i = 0;i < ts_ids.size();++i)
+                r.emplace_back(ta, fv += 1.0);
+            if (throw_exception) {
+                dlog << dlib::LINFO << "Throw from inside dtss executes!";
+                throw std::runtime_error("test exception");
+            }
+            return r;
+        };
+        server our_server(cb);
+
+        // set up the server object we have made
+        our_server.set_listening_ip("127.0.0.1");
+        int port_no = 20000;
+        our_server.set_listening_port(port_no);
+        our_server.start_async();
+        int n_threads = 10;
+
+        vector<future<void>> clients;
+        for (size_t i = 0;i < n_threads;++i) {
+            clients.emplace_back(
+                    async(launch::async, [port_no,ta,ta24,i]()         /** thread this */ {
+                    string host_port = string("localhost:") + to_string(port_no);
+                    dlog << dlib::LINFO << "sending an expression ts to " << host_port;
+                    std::vector<api::apoint_ts> tsl;
+                    for (size_t x = 1;x <= 83;++x) // just make a  very thin request, that get loads of data back
+                        tsl.push_back(double(x)*api::apoint_ts(string("netcdf://group/path/ts_") + std::to_string(x)));
+
+                    client dtss(host_port);
+                    auto t0 = timing::now();
+                    size_t eval_count = 0;
+                    size_t test_duration_ms = 10000;
+                    while (elapsed_ms(t0, timing::now()) < test_duration_ms) {
+                        // burn cpu server side, save time on serialization
+                        std::vector<int> percentile_spec{ 0,25,50,-1,75,100 };
+                        auto percentiles = dtss.percentiles(tsl, ta.total_period(), ta24, percentile_spec);
+                        //slower due to serialization: auto ts_b = dtss.evaluate(tsl, ta.total_period());
+                        ++eval_count;
+                    }
+                    auto total_ms = double(elapsed_ms(t0, timing::now()));
+                    dlog << dlib::LINFO << "Done testing " << i << ": #= " << eval_count << " e/s = " << 1000 * double(eval_count) / total_ms << " [e/s]";
+                    dtss.close();
+                }
+
+                )
+            );
+        }
+        for (auto &f : clients) f.get();
+        our_server.clear();
+        dlog << dlib::LINFO << "done";
+        
+    } catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    dlog << dlib::LINFO << "done";
+}
+
+TEST_SUITE_END();
