31711b67e92411786abe0e8dafb51c6f81338559
felixmatt <f.n.matt@geo.uio.no>
2017-04-30 12:11:39 +0200

    adding functionality to wrf repository. Tests run green.

--- shyft/repository/netcdf/wrf_data_repository.py
+++ shyft/repository/netcdf/wrf_data_repository.py
@@ -44,7 +44,7 @@ class WRFDataRepository(interfaces.GeoTsRepository):
 
     """
 
-    _G = 9.80665 #  WMO-defined gravity constant to calculate the height in metres from geopotential
+    _G = 9.80665  # WMO-defined gravity constant to calculate the height in metres from geopotential
 
     def __init__(self, epsg, directory, filename=None, bounding_box=None,
                  x_padding=5000.0, y_padding=5000.0, allow_subset=False):
@@ -91,16 +91,18 @@ class WRFDataRepository(interfaces.GeoTsRepository):
 
         # Field names and mappings
         self.wrf_shyft_map = {
-            "Q2": "relative_humidity",
             "T2": "temperature",
             "HGT": "z",
             "PREC_ACC_NC": "precipitation",
-            "U": "x_wind",
-            "V": "y_wind",
-            "SWDOWN": "radiation"}
+            "U10": "x_wind",
+            "V10": "y_wind",
+            "SWDOWN": "radiation",
+            "Q2": "mixing_ratio",
+            "PSFC": "pressure"}
 
         # Fields that need an additional timeslice because the measure average values
-        self._shift_fields = ("PREC_ACC_NC", "SWDOWN")
+        # self._shift_fields = ("PREC_ACC_NC", "SWDOWN")
+        self._shift_fields = ()
 
         self.source_type_map = {"relative_humidity": api.RelHumSource,
                                 "temperature": api.TemperatureSource,
@@ -108,7 +110,6 @@ class WRFDataRepository(interfaces.GeoTsRepository):
                                 "radiation": api.RadiationSource,
                                 "wind_speed": api.WindSpeedSource}
 
-    # TODO: We still need to adapt this to the pattern structure of WRF files
     def get_timeseries(self, input_source_types, utc_period, geo_location_criteria=None):
         """Get shyft source vectors of time series for input_source_types
 
@@ -130,71 +131,13 @@ class WRFDataRepository(interfaces.GeoTsRepository):
         filename = self._filename
         if not path.isfile(filename):
             if '*' in filename:
+                # TODO: We still need to adapt this to the pattern structure of multiple WRF files
                 filename = self._get_files(utc_period.start, "_(\d{8})([T_])(\d{2})(Z)?.nc$")
             else:
                 raise WRFDataRepositoryError("File '{}' not found".format(filename))
         with Dataset(filename) as dataset:
             return self._get_data_from_dataset(dataset, input_source_types,
                                                utc_period, geo_location_criteria)
-
-    def get_forecast(self, input_source_types, utc_period, t_c, geo_location_criteria=None):
-        """
-        Parameters
-        ----------
-        input_source_types: list
-            List of source types to retrieve. Valid types are:
-                * relative_humidity
-                * temperature
-                * precipitation
-                * radiation
-                * wind_speed
-        utc_period: api.UtcPeriod
-            The utc time period that should (as a minimum) be covered.
-        t_c: long
-            Forecast specification; return newest forecast older than t_c.
-        geo_location_criteria: object
-            Some type (to be decided), extent (bbox + coord.ref).
-
-        Returns
-        -------
-        geo_loc_ts: dictionary
-            dictionary keyed by ts type, where values are api vectors of geo
-            located timeseries.
-        """
-        filename = self._get_files(t_c, "_(\d{8})([T_])(\d{2})(Z)?.nc$")
-        with Dataset(filename) as dataset:
-            return self._get_data_from_dataset(dataset, input_source_types, utc_period,
-                                               geo_location_criteria)
-
-    def get_forecast_ensemble(self, input_source_types, utc_period,
-                              t_c, geo_location_criteria=None):
-        """
-        Parameters
-        ----------
-        input_source_types: list
-            List of source types to retrieve (precipitation, temperature, ...)
-        utc_period: api.UtcPeriod
-            The utc time period that should (as a minimum) be covered.
-        t_c: long
-            Forecast specification; return newest forecast older than t_c.
-        geo_location_criteria: object
-            Some type (to be decided), extent (bbox + coord.ref).
-
-        Returns
-        -------
-        ensemble: list of geo_loc_ts dictionaries
-            Dictionaries are keyed by time series type, with values
-            being api vectors of geo located timeseries.
-        """
-
-        filename = self._get_files(t_c, "\D(\d{8})(\d{2}).nc$")
-        with Dataset(filename) as dataset:
-            res = []
-            for idx in dataset.variables["ensemble_member"][:]:
-                res.append(self._get_data_from_dataset(dataset, input_source_types, utc_period,
-                                                       geo_location_criteria,
-                                                       ensemble_member=idx))
-            return res
 
     @property
     def bounding_box(self):
@@ -229,18 +172,14 @@ class WRFDataRepository(interfaces.GeoTsRepository):
         tsc = api.TsFactory().create_point_ts
         time_series = {}
         for key, (data, ta) in data.items():
-            fslice = (len(data.shape) - 2)*[slice(None)]
-            I, J = data.shape[-2:]
-
             def construct(d):
                 if ta.size() != d.size:
                     raise WRFDataRepositoryError("Time axis size {} not equal to the number of "
-                                                   "data points ({}) for {}"
-                                                   "".format(ta.size(), d.size, key))
+                                                        "data points ({}) for {}"
+                                                        "".format(ta.size(), d.size, key))
                 return tsc(ta.size(), ta.start, ta.delta_t,
-                           api.DoubleVector_FromNdArray(d.flatten()), api.point_interpretation_policy.POINT_AVERAGE_VALUE)
-            time_series[key] = np.array([[construct(data[fslice + [i, j]])
-                                          for j in range(J)] for i in range(I)])
+                           api.DoubleVector_FromNdArray(d), api.point_interpretation_policy.POINT_AVERAGE_VALUE)
+            time_series[key] = np.array([construct(data[:,i]) for i in range(data.shape[1])])
         return time_series
 
     def _limit(self, x, y, data_cs, target_cs):
@@ -274,30 +213,61 @@ class WRFDataRepository(interfaces.GeoTsRepository):
         target_proj = Proj(target_cs)
 
         # Find bounding box in WRF projection
+        x_targ, y_targ = transform(data_proj, target_proj, x, y)
         bbox = self.bounding_box
-        bb_proj = transform(target_proj, data_proj, bbox[0], bbox[1])
-        x_min, x_max = min(bb_proj[0]), max(bb_proj[0])
-        y_min, y_max = min(bb_proj[1]), max(bb_proj[1])
-
+        x_min, x_max = min(bbox[0]), max(bbox[0])
+        y_min, y_max = min(bbox[1]), max(bbox[1])
         # Mask for the limits
-        mask = ((x >= x_min) & (x <= x_max) & (y >= y_min) & (y <= y_max))
+        mask = ((x_targ >= x_min) & (x_targ <= x_max) & (y_targ >= y_min) & (y_targ <= y_max))
+
+        if not mask.any():
+            raise WRFDataRepositoryError("Bounding box doesn't intersect with dataset.")
 
         # Transform from source coordinates to target coordinates
-        xx, yy = transform(data_proj, target_proj, x[mask], y[mask])
-        y_mask, x_mask = np.nonzero(mask)
-        return xx, yy, (x_mask, y_mask)
+        xx, yy = x_targ[mask], y_targ[mask]
+        return xx, yy, mask
+
+    def _calculate_rel_hum(self, T2, PSFC, Q2):
+        # constants
+        EZERO = 6.112
+        ESLCON1 = 17.67
+        ESLCON2 = 29.65
+        CELKEL = 273.15
+        RD = 287.
+        RV = 461.6
+        EPS = 0.622
+
+        # calculation
+        RH = np.empty_like(T2)
+        es = EZERO * np.exp(ESLCON1 * (T2 - CELKEL) / (T2 - ESLCON2))
+        qvs = EPS * es / (0.01 * PSFC - (1.0 - EPS) * es)
+        RH = Q2 / qvs
+        RH[RH > 1.0] = 1.0
+        RH[RH < 0.0] = 0.0
+        return RH
 
     def _get_data_from_dataset(self, dataset, input_source_types, utc_period,
                                geo_location_criteria, ensemble_member=None):
 
+
         if geo_location_criteria is not None:
             self._bounding_box = geo_location_criteria
 
+        input_source_types_orig = list(input_source_types)
         if "wind_speed" in input_source_types:
             input_source_types = list(input_source_types)  # We change input list, so take a copy
             input_source_types.remove("wind_speed")
             input_source_types.append("x_wind")
             input_source_types.append("y_wind")
+
+        if "relative_humidity" in input_source_types:
+            input_source_types = list(input_source_types)  # We change input list, so take a copy
+            input_source_types.remove("relative_humidity")
+            input_source_types.append("mixing_ratio")
+            input_source_types.append("pressure")
+            if not "temperature" in input_source_types:
+                input_source_types.append("temperature") # Needed for rel_hum calculation
+
 
         raw_data = {}
         x = dataset.variables.get("XLONG", None)
@@ -306,6 +276,9 @@ class WRFDataRepository(interfaces.GeoTsRepository):
         if not all([x, y, time]):
             raise WRFDataRepositoryError("Something is wrong with the dataset."
                                            " x/y coords or time not found.")
+        # TODO: make a check that dim1 is time, dim2 is ..., dim3 is ...
+        x = x[0,:,:].reshape(x.shape[1] * x.shape[2])
+        y = y[0,:,:].reshape(y.shape[1] * y.shape[2])
         time = convert_netcdf_time(time.units,time)
         # TODO: Make sure that "latlong" is the correct coordinate system in WRF data
         #data_cs_proj4 = "+proj=lcc +lon_0=78.9356 +lat_0=31.6857 +lat_1=30 +lat_2=60 +R=6.371e+06 +units=m +no_defs"
@@ -317,49 +290,51 @@ class WRFDataRepository(interfaces.GeoTsRepository):
         idx_max = np.searchsorted(time, utc_period.end, side='right')
         issubset = True if idx_max < len(time) - 1 else False
         time_slice = slice(idx_min, idx_max)
-        x, y, (m_x, m_y) = self._limit(x[0], y[0], data_cs_proj4, self.shyft_cs)
+        x, y, mask = self._limit(x, y , data_cs_proj4, self.shyft_cs)
         for k in dataset.variables.keys():
-            if k in ('U', 'V'):  # skip wind variables for now
-                continue
             if self.wrf_shyft_map.get(k, None) in input_source_types:
                 if k in self._shift_fields and issubset:  # Add one to time slice
                     data_time_slice = slice(time_slice.start, time_slice.stop + 1)
                 else:
                     data_time_slice = time_slice
                 data = dataset.variables[k]
-                dims = data.dimensions
-                data_slice = len(data.dimensions)*[slice(None)]
-                if ensemble_member is not None:
-                    data_slice[dims.index("ensemble_member")] = ensemble_member
-                data_slice[dims.index("west_east")] = m_x
-                data_slice[dims.index("south_north")] = m_y
-                data_slice[dims.index("Time")] = data_time_slice
-                pure_arr = data[:][data_slice]
+                # TODO: make a check that data's dim1 is time, dim2 is ..., dim3 is ...
+                data = data[:].reshape(data.shape[0], data.shape[1] * data.shape[2])
+                data_slice = [slice(None), slice(None)]
+                #if ensemble_member is not None:
+                #    data_slice[dims.index("ensemble_member")] = ensemble_member
+                data_slice[0] = data_time_slice
+                data_slice[1] = mask
+                pure_arr = data[data_slice]
                 if isinstance(pure_arr, np.ma.core.MaskedArray):
-                    #print(pure_arr.fill_value)
                     pure_arr = pure_arr.filled(np.nan)
                 raw_data[self.wrf_shyft_map[k]] = pure_arr, k
 
         if 'HGT' in dataset.variables.keys():
             data = dataset.variables['HGT']
-            dims = data.dimensions
-            data_slice = [None, None]
-            data_slice[dims.index("west_east") - 1] = m_x
-            data_slice[dims.index("south_north") - 1] = m_y
-            z = data[0][data_slice]  # get the first entry in time
+            data = data[0, :, :].reshape(data.shape[1] * data.shape[2]) # get the first entry in time
+            z = data[mask]
         else:
             raise WRFDataRepositoryError("No elevations found in dataset.")
 
-        pts = np.dstack((x, y, z)).reshape(*(x.shape + (3,)))
+        pts = np.stack((x, y, z), axis=-1)
+
 
         # Make sure requested fields are valid, and that dataset contains the requested data.
         if not self.allow_subset and not (set(raw_data.keys()).issuperset(input_source_types)):
             raise WRFDataRepositoryError("Could not find all data fields")
-
         if {"x_wind", "y_wind"}.issubset(raw_data):
             x_wind, _ = raw_data.pop("x_wind")
             y_wind, _ = raw_data.pop("y_wind")
             raw_data["wind_speed"] = np.sqrt(np.square(x_wind) + np.square(y_wind)), "wind_speed"
+        if {"temperature", "pressure", "mixing_ratio"}.issubset(raw_data):
+            pressure, _ = raw_data.pop("pressure")
+            mixing_ratio, _ = raw_data.pop("mixing_ratio")
+            if "temperature" in input_source_types_orig:
+                temperature, _ = raw_data["temperature"]  # Temperature input requested
+            else:
+                temperature, _ = raw_data.pop("temperature")  # Temperature only needed for relhum calculation
+            raw_data["relative_humidity"] = self._calculate_rel_hum(temperature, pressure, mixing_ratio), "relative_humidity_2m"
         extracted_data = self._transform_raw(raw_data, time[time_slice], issubset=issubset)
         return self._geo_ts_to_vec(self._convert_to_timeseries(extracted_data), pts)
 
@@ -382,27 +357,29 @@ class WRFDataRepository(interfaces.GeoTsRepository):
             return x
 
         def air_temp_conv(T):
-            return T - 273.15
+            return T - 273.16
 
         def prec_conv(p):
-            return p[1:]
+            #return p[1:]
+            return p
 
-        def prec_acc_conv(p):
-            return np.clip(p[1:] - p[:-1], 0.0, 1000.0)
+        #def prec_acc_conv(p):
+        #    return np.clip(p[1:] - p[:-1], 0.0, 1000.0)
 
         def rad_conv(r):
-            dr = r[1:] - r[:-1]
-            return np.clip(dr/(time[1] - time[0]), 0.0, 5000.0)
+            #dr = r[1:] - r[:-1]
+            #return np.clip(dr/(time[1] - time[0]), 0.0, 5000.0)
+            return r
 
         convert_map = {"wind_speed": lambda x, t: (noop_space(x), noop_time(t)),
                        "relative_humidity_2m": lambda x, t: (noop_space(x), noop_time(t)),
-                       "air_temperature_2m": lambda x, t: (air_temp_conv(x), noop_time(t)),
-                       "integral_of_surface_downwelling_shortwave_flux_in_air_wrt_time":
-                       lambda x, t: (rad_conv(x), dacc_time(t)),
-                       "precipitation_amount": lambda x, t: (prec_conv(x), dacc_time(t)),
-                       "precipitation_amount_acc": lambda x, t: (prec_acc_conv(x), dacc_time(t))}
+                       "T2": lambda x, t: (air_temp_conv(x), noop_time(t)),
+                       "SWDOWN": lambda x, t: (rad_conv(x), noop_time(t)),
+                       "PREC_ACC_NC": lambda x, t: (prec_conv(x), noop_time(t))}
+                       #"precipitation_amount_acc": lambda x, t: (prec_acc_conv(x), dacc_time(t))}
         res = {}
         for k, (v, ak) in data.items():
+
             res[k] = convert_map[ak](v, time)
         return res
 
@@ -415,9 +392,8 @@ class WRFDataRepository(interfaces.GeoTsRepository):
             #res[name] = tpe.vector_t([tpe(api.GeoPoint(*pts[idx]), ts[idx]) for idx in np.ndindex(pts.shape[:-1])])
             #     so until then, we have to do the loop
             tpe_v=tpe.vector_t()
-            for idx in np.ndindex(pts.shape[:-1]):
+            for idx in range(len(ts)):
                 tpe_v.append(tpe(api.GeoPoint(*pts[idx]), ts[idx]))
-
             res[name] = tpe_v
         return res
 
