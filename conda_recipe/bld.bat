bf027ea659cb93588d946f7aa3e3e8b55bbb2a5e
sigbjorn <sigbjorn.helset@gmail.com>
2017-09-09 15:00:50 +0200

    Conda support (#175)
    * remove broken bdist_conda, add conda_recipe folder with startup files
    
    * more to ignore
    
    * added some support for version-setting, target py 3.6 np 1.13

---
+++ conda_recipe/bld.bat
@@ -0,0 +1,3 @@
+echo --- environment ---
+%PYTHON% setup.py install
+if errorlevel 1 exit 1
--- setup.py
+++ setup.py
@@ -6,63 +6,71 @@ import glob
 import platform
 import subprocess
 from setuptools import setup, find_packages
-bdist_class=None
-try:
-	import distutils.command.bdist_conda
-	bdist_class=distutils.command.bdist_conda.CondaDistribution
-except:
-	pass
 
 
 print('Building SHyFT')
-
-if "Windows" in platform.platform():
-    msbuild_2015 = r'C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe' if 'MSBUILD_2015_PATH' not in os.environ else os.environ['MSBUILD_2015_PATH']
-    msbuild_2017 = r'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\MSBuild.exe' if 'MSBUILD_2017_PATH' not in os.environ else os.environ['MSBUILD_2017_PATH']
-    if path.exists(msbuild_2017):
-        msbuild = msbuild_2017
-        cmd = [msbuild, '/p:Configuration=Release', '/p:Platform=x64','/p:PlatformToolset=v141', '/p:WindowsTargetPlatformVersion=10.0.14393.0', '/m']
-    elif path.exists(msbuild_2015):
-        msbuild = msbuild_2015
-        cmd = [msbuild, '/p:Configuration=Release', '/p:Platform=x64', '/m']
-    else:
-        print("Sorry, but this setup only supports ms c++ installed to standard locations")
-        print(" you can set MSBUILD_2015_PATH or MSBUILD_2017_PATH specific to your installation and restart.")
-        exit()
-    
-    if '--rebuild' in sys.argv:
-        cmd.append('/t:Rebuild')
-        sys.argv.remove('--rebuild')
-    
-    p = subprocess.Popen(cmd,
-        universal_newlines=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-
-    for line in iter(p.stdout.readline, ''):
-       print(line.rstrip())
-    
-    p.wait()
-    if p.returncode != 0:
-        print('\nMSBuild FAILED.')
-        exit()
-    
-elif "Linux" in platform.platform():
-    try:
-    # For Linux, use the cmake approach for compiling the extensions
-        print(subprocess.check_output("sh build_api_cmake.sh", shell=True))
-    except:
-        print("Problems compiling shyft, try building with the build_api.sh "
-              "or build_api_cmake.sh (Linux only) script manually...")
-        exit()
-else:
-    print("Only windows and Linux supported")
-    exit()
 
 # VERSION should be set in a previous build step (ex: TeamCity)
 VERSION = open('VERSION').read().strip()
 # Create the version.py file
 open('shyft/version.py', 'w').write('__version__ = "%s"\n' % VERSION)
+
+ext_s = '.pyd' if  'Windows' in platform.platform() else '.so'
+
+ext_names=['shyft/api/_api'+ext_s,
+           'shyft/api/pt_gs_k/_pt_gs_k'+ext_s,
+           'shyft/api/pt_hs_k/_pt_hs_k'+ext_s,
+           'shyft/api/pt_ss_k/_pt_ss_k'+ext_s,
+           'shyft/api/hbv_stack/_hbv_stack'+ext_s ]
+           
+needs_build_ext = not all([path.exists(ext_name) for ext_name in ext_names])
+
+if needs_build_ext:
+    print('One or more extension modules needs build, attempting auto build')
+    if "Windows" in platform.platform():
+        msbuild_2015 = r'C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe' if 'MSBUILD_2015_PATH' not in os.environ else os.environ['MSBUILD_2015_PATH']
+        msbuild_2017 = r'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\MSBuild.exe' if 'MSBUILD_2017_PATH' not in os.environ else os.environ['MSBUILD_2017_PATH']
+        if path.exists(msbuild_2017):
+            msbuild = msbuild_2017
+            cmd = [msbuild, '/p:Configuration=Release', '/p:Platform=x64','/p:PlatformToolset=v141', '/p:WindowsTargetPlatformVersion=10.0.14393.0', '/m']
+        elif path.exists(msbuild_2015):
+            msbuild = msbuild_2015
+            cmd = [msbuild, '/p:Configuration=Release', '/p:Platform=x64', '/m']
+        else:
+            print("Sorry, but this setup only supports ms c++ installed to standard locations")
+            print(" you can set MSBUILD_2015_PATH or MSBUILD_2017_PATH specific to your installation and restart.")
+            exit()
+        
+        if '--rebuild' in sys.argv:
+            cmd.append('/t:Rebuild')
+            sys.argv.remove('--rebuild')
+        
+        p = subprocess.Popen(cmd,
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+
+        for line in iter(p.stdout.readline, ''):
+           print(line.rstrip())
+        
+        p.wait()
+        if p.returncode != 0:
+            print('\nMSBuild FAILED.')
+            exit()
+        
+    elif "Linux" in platform.platform():
+        try:
+        # For Linux, use the cmake approach for compiling the extensions
+            print(subprocess.check_output("sh build_api_cmake.sh", shell=True))
+        except:
+            print("Problems compiling shyft, try building with the build_api.sh "
+                  "or build_api_cmake.sh (Linux only) script manually...")
+            exit()
+    else:
+        print("Only windows and Linux supported")
+        exit()
+else:
+    print('Extension modules are already built in place')
 
 # Copy libraries needed to run Shyft
 if "Windows" in platform.platform():
@@ -79,40 +87,19 @@ if "Windows" in platform.platform():
         shutil.copy2(f, path.join(dest_dir, path.basename(f)))
 
 requires = ["numpy", "nose", "netcdf4", "pyyaml","six", "pyproj", "shapely" ]
-if bdist_class:
-	setup(
-		name='shyft',
-		version=VERSION,
-		distclass=bdist_class,
-		author='Statkraft',
-		author_email='shyft@statkraft.com',
-		url='https://github.com/statkraft/shyft',
-		description='An OpenSource hydrological toolbox',
-		license='LGPL v3',
-		packages=find_packages(),
-		package_data={'shyft': ['api/*.so', 'api/*.pyd', 'api/pt_gs_k/*.pyd', 'api/pt_gs_k/*.so', 'api/pt_hs_k/*.pyd', 
-			'api/pt_hs_k/*.so', 'api/pt_ss_k/*.pyd', 'api/pt_ss_k/*.so', 'api/hbv_stack/*.pyd', 'api/hbv_stack/*.so', 
-			'tests/netcdf/*', 'lib/*.dll']},
-		entry_points={},
-		requires= requires,
-		install_requires=requires,
-		conda_import_tests=False
-	)
-else:
-	setup(
-		name='shyft',
-		version=VERSION,
-		distclass=bdist_class,
-		author='Statkraft',
-		author_email='shyft@statkraft.com',
-		url='https://github.com/statkraft/shyft',
-		description='An OpenSource hydrological toolbox',
-		license='LGPL v3',
-		packages=find_packages(),
-		package_data={'shyft': ['api/*.so', 'api/*.pyd', 'api/pt_gs_k/*.pyd', 'api/pt_gs_k/*.so', 'api/pt_hs_k/*.pyd', 
-			'api/pt_hs_k/*.so', 'api/pt_ss_k/*.pyd', 'api/pt_ss_k/*.so', 'api/hbv_stack/*.pyd', 'api/hbv_stack/*.so', 
-			'tests/netcdf/*', 'lib/*.dll']},
-		entry_points={},
-		requires= requires,
-		install_requires=requires,
-		)
+setup(
+    name='shyft',
+    version=VERSION,
+    author='Statkraft',
+    author_email='shyft@statkraft.com',
+    url='https://github.com/statkraft/shyft',
+    description='An OpenSource hydrological toolbox',
+    license='LGPL v3',
+    packages=find_packages(),
+    package_data={'shyft': ['api/*.so', 'api/*.pyd', 'api/pt_gs_k/*.pyd', 'api/pt_gs_k/*.so', 'api/pt_hs_k/*.pyd', 
+        'api/pt_hs_k/*.so', 'api/pt_ss_k/*.pyd', 'api/pt_ss_k/*.so', 'api/hbv_stack/*.pyd', 'api/hbv_stack/*.so', 
+        'tests/netcdf/*', 'lib/*.dll']},
+    entry_points={},
+    requires= requires,
+    install_requires=requires,
+    )
