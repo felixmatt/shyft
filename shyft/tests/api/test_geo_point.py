23124df492816263c72e8727322b95e122c7bc38
sigbjorn <sigbjorn.helset@gmail.com>
2018-01-15 21:46:12 +0100

    added missing test/demo for GeoPointVector.create_from_x_y_z

--- shyft/tests/api/test_geo_point.py
+++ shyft/tests/api/test_geo_point.py
@@ -8,31 +8,42 @@ class GeoPoint(unittest.TestCase):
        cells, and when you have geo located time-series that
        you feed into shyft
      """
-    
+
     def test_create(self):
-        p1=api.GeoPoint(1,2,3)
-       
-        self.assertAlmostEqual(p1.x,1)
-        self.assertAlmostEqual(p1.y,2)
-        self.assertAlmostEqual(p1.z,3)
+        p1 = api.GeoPoint(1, 2, 3)
+
+        self.assertAlmostEqual(p1.x, 1)
+        self.assertAlmostEqual(p1.y, 2)
+        self.assertAlmostEqual(p1.z, 3)
 
     def test_create_default(self):
-        p2=api.GeoPoint()
-        self.assertAlmostEqual(p2.x,0)
-        self.assertAlmostEqual(p2.y,0)
-        self.assertAlmostEqual(p2.z,0)
-    
+        p2 = api.GeoPoint()
+        self.assertAlmostEqual(p2.x, 0)
+        self.assertAlmostEqual(p2.y, 0)
+        self.assertAlmostEqual(p2.z, 0)
+
     def test_difference_of_two(self):
-        a=api.GeoPoint(1,2,3)
-        b=api.GeoPoint(3,4,8)
-        d=api.GeoPoint_difference(b,a)
-        self.assertAlmostEqual(d.x,b.x-a.x)
-        
+        a = api.GeoPoint(1, 2, 3)
+        b = api.GeoPoint(3, 4, 8)
+        d = api.GeoPoint_difference(b, a)
+        self.assertAlmostEqual(d.x, b.x - a.x)
+
     def test_xy_distance(self):
-        a=api.GeoPoint(1,2,3)
-        b=api.GeoPoint(3,4,8)
-        d=api.GeoPoint_xy_distance(b,a)
-        self.assertAlmostEqual(d,2.8284271247 )
-        
+        a = api.GeoPoint(1, 2, 3)
+        b = api.GeoPoint(3, 4, 8)
+        d = api.GeoPoint_xy_distance(b, a)
+        self.assertAlmostEqual(d, 2.8284271247)
+
+    def test_create_from_x_y_z_vector(self):
+        x = api.DoubleVector([1.0, 4.0, 7.0])
+        y = api.DoubleVector([2.0, 5.0, 8.0])
+        z = api.DoubleVector([3.0, 6.0, 9.0])
+        gpv = api.GeoPointVector.create_from_x_y_z(x, y, z)
+        for i in range(3):
+            self.assertAlmostEqual(gpv[i].x, 3 * i + 1)
+            self.assertAlmostEqual(gpv[i].y, 3 * i + 2)
+            self.assertAlmostEqual(gpv[i].z, 3 * i + 3)
+
+
 if __name__ == "__main__":
     unittest.main()
